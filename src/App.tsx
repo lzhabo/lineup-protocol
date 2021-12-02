a6b21cebe0c5003486f2872ef17d839b3137d34e
lzhabo <lidia.zhabo99@gmail.com>
2021-12-02 16:22:42 +0300

    added some packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "@emotion/styled";
+import React from "react";
+import { ReactComponent as Logo } from "@src/assets/bigLogo.svg";
 
-function App() {
+interface IProps {}
+
+const Root = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: black;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const App: React.FC<IProps> = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Root>
+      <Logo />
+    </Root>
   );
-}
-
+};
 export default App;
