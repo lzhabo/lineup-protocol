907b5365c991ff9ee08997c6c9748bcb3ff7d4e0
Олексiй <alexnagorny.an@gmail.com>
2021-12-03 11:27:22 +0300

    tokeninput

--- src/components/TokenInput/TokenSelect.tsx
+++ src/components/TokenInput/TokenSelect.tsx
@@ -1,21 +1,41 @@
 import styled from "@emotion/styled";
 import React from "react";
+import { ReactComponent as Btc } from "@src/assets/images/bitcoin.svg";
+import { ReactComponent as Arrow } from "@src/assets/icons/arrowDown.svg";
+import { Row } from "../Flex";
 
 interface IProps {}
 
 const Root = styled.div`
   display: flex;
-  flex-direction: column;
-
+  align-items: center;
   height: 40px;
-  width: 160px;
+  max-width: 160px;
+  width: 100%;
   padding: 8px;
   background: #0b0b0d;
   border-radius: 20px;
   box-sizing: border-box;
 `;
 
+const TokenName = styled.div`
+  font-family: Poppins, sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 20px;
+  color: #ffffff;
+`;
+
 const TokenSelect: React.FC<IProps> = () => {
-  return <Root></Root>;
+  return (
+    <Root>
+      <Row alignItems="center">
+        <Btc style={{ borderRadius: "50%", marginRight: 8 }} />
+        <TokenName>BTC</TokenName>
+      </Row>
+      <Arrow style={{ justifySelf: "flex-end", marginRight: 12 }} />
+    </Root>
+  );
 };
 export default TokenSelect;
--- src/components/TokenInput/index.tsx
+++ src/components/TokenInput/index.tsx
@@ -1,7 +1,11 @@
 import styled from "@emotion/styled";
 import React from "react";
-import { Row } from "@components/flex";
+import { Row } from "@components/Flex";
 import TokenSelect from "@components/TokenInput/TokenSelect";
+import BigNumberInput from "@components/BigNumberInput";
+import BN from "@src/utils/BN";
+import AmountInput from "@components/TokenInput/AmountInput";
+import SizedBox from "@components/SizedBox";
 
 interface IProps {}
 
@@ -20,9 +24,17 @@ const Divider = styled.div`
 const TokenInput: React.FC<IProps> = () => {
   return (
     <Root>
-      <Row>
+      <Row alignItems="center" justifyContent="space-between">
         <TokenSelect />
+        <SizedBox width={8} />
+        <BigNumberInput
+          decimals={18}
+          renderInput={(props, ref) => <AmountInput {...props} ref={ref} />}
+          value={new BN(1.12).times(1e18)}
+          onChange={(n) => null}
+        />
       </Row>
+      <SizedBox height={8} />
       <Divider />
     </Root>
   );
