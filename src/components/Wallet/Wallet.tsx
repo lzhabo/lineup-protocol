209c0130415ce320be15ca89cedaa5c28bfeb3cf
Олексiй <alexnagorny.an@gmail.com>
2022-03-18 00:29:36 +0300

    :recycle: remove unused InjectedConnector ussage

--- src/components/Wallet/Wallet.tsx
+++ src/components/Wallet/Wallet.tsx
@@ -4,13 +4,7 @@ import { useStores } from "@stores";
 import { observer } from "mobx-react-lite";
 import LoginModal from "./LoginModal";
 import LoggedInAccountInfo from "@components/Wallet/LoggedInAccountInfo";
-
-import { InjectedConnector } from "@web3-react/injected-connector";
 import Button from "@components/Button";
-
-export const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42],
-});
 
 interface IProps {}
 
